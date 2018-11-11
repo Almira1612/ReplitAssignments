9147e83388e6e4b76ea6559d28fd488fe1a2e19b
Almira1612 <almira1612@gmail.com>
2018-11-10 22:23:03 -0500

    Added 6

---
+++ OOP/OOP 6 - carpet class - Carpet.java
@@ -0,0 +1,25 @@
+public class Carpet {
+ 
+ //do not change -- Start
+  public double width,length,unitPrice,totalPrice;
+  public boolean isPersian;
+ //do not change -- End
+ 
+ //Add constructors below
+  public Carpet(){
+    this.width = 300;
+    this.length=300;
+    this.totalPrice = 200;
+    this.unitPrice = 0;
+    this.isPersian = false;
+  }
+  public Carpet(double width,double length,double unitPrice,boolean isPersian){
+    this.width = width;
+    this.length = length;
+    this.unitPrice = unitPrice;
+    this.isPersian = isPersian;
+    this.totalPrice = (width+length)*unitPrice;
+    if(isPersian) totalPrice+=200;
+  }
+  
+}
